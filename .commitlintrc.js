2bd76b5ef71bc95989e8d93718167c512a74d67a
svrnm <neumanns@cisco.com>
2021-07-02 11:20:52 +0200

    feat: add script to validate website docs.

---
+++ .commitlintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'header-max-length': [1, 'always', 72],
+        'header-full-stop': [2, 'always'],
+        'subject-full-stop': [0, 'always'],
+        'type-enum': [
+            2,
+            'always',
+            ['build', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'style', 'test', 'revert']
+        ]
+    }
+}
