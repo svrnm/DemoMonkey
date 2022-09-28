f626db33422a525f3eb526efab636d2fbb7483f4
svrnm <neumanns@cisco.com>
2022-09-28 09:19:39 +0200

    fix: remove requirement to have fullstop from commits

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -2,8 +2,6 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'header-max-length': [1, 'always', 72],
-    'header-full-stop': [2, 'always'],
-    'subject-full-stop': [0, 'always'],
     'type-enum': [
       2,
       'always',
